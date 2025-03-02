8d4dd87238f754c05582a168c81b175606eb8e97
Cindy <cindysy3@gmail.com>
2025-03-02 14:40:56 -0800

    Add environment variables

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from sqlalchemy import Integer, String, ForeignKey, Boolean
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 import uuid
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
 
 # CREATE DATABASE
@@ -16,7 +17,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///todo.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
